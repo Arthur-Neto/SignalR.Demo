667b6936446d0fe711421064e72d55c558c0baf8
Arthur Neto <arthursan.neto@hotmail.com>
2023-05-17 16:53:28 -0300

    Adicionado Angular Material

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,29 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { SignalrService } from './signalr.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    title = 'signalr-demo';
-
-    hubHelloMessage?: string;
-
-    constructor(public signalrService: SignalrService) {
-        this.hubHelloMessage = '';
-    }
-
-    public ngOnInit(): void {
-        this.signalrService.hubHelloMessage.subscribe((hubHelloMessage: string) => {
-            this.hubHelloMessage = hubHelloMessage;
-        });
-
-        this.signalrService.connection.invoke('Hello').catch((error: any) => {
-            console.log(`SignalrDemoHub.Hello() error: ${error}`);
-
-            alert('SignalrDemoHub.Hello() error!, see console for details.');
-        });
-    }
-}
+export class AppComponent {}
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,22 +1,12 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignalrService } from './signalr.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
-    providers: [
-        SignalrService,
-        {
-            provide: APP_INITIALIZER,
-            useFactory: (signalrService: SignalrService) => () => signalrService.initiateSignalrConnection(),
-            deps: [SignalrService],
-            multi: true,
-        },
-    ],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
